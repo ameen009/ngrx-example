4f28a6e10cd8b57116491c9946611a5d00bfbbfe
Mohammad Ameen Bari <mohammad.ameen.bari@sap.com>
2023-09-22 15:46:12 +0530

    second

--- crud-app/crud-app-ngrx/src/app/app.module.ts
+++ crud-app/crud-app-ngrx/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { BookStoreModule } from './store/book/book-store.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        StoreModule.forRoot(),
+        EffectsModule.forRoot(),
+        StoreDevtoolsModule.instrument({}),
+        BookStoreModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
